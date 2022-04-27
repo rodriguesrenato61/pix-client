bd90f317458b90e2536743be870c60d4b98311b5
rodriguesrenato61 <rodriguesrenato61@gmail.com>
2022-04-27 00:07:44 -0300

    Instruções de instalação 3

--- banco.sql
+++ banco.sql
@@ -9,7 +9,7 @@ CREATE TABLE pix_aplicacao(
 );
 
 INSERT INTO pix_aplicacao(id, nome, app_cod, app_key)
-    VALUES(1, 'Pix Client', 'APP_0996051650033447', '0516D7417F48E15E54B0F94B2E04B494');
+    VALUES(1, 'Pix Client', 'APP_0996051650033447', '0516D7417F48E15E54B0F94B2E04B495');
 
 CREATE TABLE pix_recebedores(
     id INTEGER NOT NULL,
